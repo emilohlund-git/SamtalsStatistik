1a3b60607d77e1e6b6c1597511bc0d8ccabf04a1
emilohlund-git <51457329+emilohlund-git@users.noreply.github.com>
2019-08-27 08:03:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -133,9 +133,9 @@ function toggleRows(text) {
 
 	for (i = 0; i < kategorier.length; i++) {
 		if (clicked) {
-			deactivateBadge(räknareText[i]);
+			deactivateBadge(raknareText[i]);
 		} else {
-			activateBadge(räknareText[i]);
+			activateBadge(raknareText[i]);
 		}
 	}
 
@@ -195,14 +195,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function deactivateBadge(id) {
 	document.getElementById(id).style.opacity = .5;
-	if (document.getElementById(id).id == document.getElementById(räknareText[räknareText.length-1]).id) {
+	if (document.getElementById(id).id == document.getElementById(räknareText[raknareText.length-1]).id) {
 		clicked = false;
 	}
 }
 
 function activateBadge(id) {
 	document.getElementById(id).style.opacity = 1;
-	if (document.getElementById(id).id == document.getElementById(räknareText[räknareText.length-1]).id) {
+	if (document.getElementById(id).id == document.getElementById(räknareText[raknareText.length-1]).id) {
 		clicked = true;
 	}
 }
